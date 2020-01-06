d834575ae5563091a5b4085883a83b1e01b5f412
Vijay Sivasubramanian <37892349+vijshia@users.noreply.github.com>
2020-01-06 13:13:06 +0530

    Update batchestobeScannedPage.java
    updating batchestobeScannedPage

--- eclipse-workspace/TestArt/src/test/java/com/FA/app/pages/fa/batchestobeScannedPage.java
+++ eclipse-workspace/TestArt/src/test/java/com/FA/app/pages/fa/batchestobeScannedPage.java
@@ -8,16 +8,17 @@ import org.openqa.selenium.By;
 import com.FA.app.pages.basePage;
 import com.FA.framework.Log;
 
-import static com.FA.app.pages.fa.batchEntryPage.menu_Batch;
+import static com.FA.app.pages.fa.piBatchPage.elementtoInvisible;
 import static com.FA.framework.WebContext.batchCode;
-import static com.FA.app.tests.test.ChargeBatchTest.BS_StartingDocumentNumber;
+import static com.FA.app.tests.baseTest.hm_testdata;
 /**
  * @author ajp16088
  *
  */
 public class batchestobeScannedPage extends basePage{
 	
-	private By menu_BatchestobeScanned = By.xpath("//a[text()='Batches to be Scanned']");
+//	private By menu_BatchestobeScanned = By.xpath("//a[text()='Batches to be Scanned']");
+	private By widget_ScanBatch = By.xpath("//li[contains(@onclick,'scanBatchSelectNew')]");
 	private By header_Grid = By.id("fixed_header");
 	private By table_value = By.xpath("//a[contains(@href,'editBatchScan')]");
 //	private By lnk_sNo = By.xpath("//*[text()='" + batchCode + "']/parent::*");
@@ -32,13 +33,19 @@ public class batchestobeScannedPage extends basePage{
 	public static By msg_BatchSavedConfirmation = By.xpath("//*[@id='saveInProgress']/..");
 	public static By btn_piBatches_search = By.name("Search");
 	
+	batchEntryPage batchentrypage = new batchEntryPage();
+	
 	public void NavigatetoBatchestobeScanned() throws Exception {
 
-		javascript_scrollIntoView(menu_Batch);
-		action_movetoElementandClick(menu_Batch);
-		waitforVisibilityofElementLocated(menu_BatchestobeScanned, 20);
-		clickonButton(menu_BatchestobeScanned);
-		waitforVisibilityofElementLocated(txt_joborBatchCode, 15);
+		Log.info(" *************** Batches tobe Scanned Execution Started *************** ");
+		/*
+		 * javascript_scrollIntoView(menu_Batch);
+		 * action_movetoElementandClick(menu_Batch);
+		 * waitforVisibilityofElementLocated(menu_BatchestobeScanned, 20);
+		 * clickonButton(menu_BatchestobeScanned);
+		 * waitforVisibilityofElementLocated(txt_joborBatchCode, 15);
+		 */
+		NavigatetoCreateBatch();
 		enteringValues(txt_joborBatchCode, batchCode);
 		clickonButton(btn_piBatches_search);
 		waitforVisibilityofElementLocated(header_Grid, 20);
@@ -48,15 +55,34 @@ public class batchestobeScannedPage extends basePage{
 		waitforVisibilityofElementLocated(chkbox_hasBeenScanned, 10);
 		clickonButton(chkbox_hasBeenScanned);
 		waitforVisibilityofElementLocated(txt_StartDCN, 10);
-		enteringValues(txt_StartDCN, BS_StartingDocumentNumber);
+		if(hm_testdata.get("BE_DocumentCount").contentEquals("1")) {
+			enteringValues(txt_StartDCN, hm_testdata.get("BS_StartingDocumentNumber"));
+		} else {
+			enteringValues(txt_StartDCN, generateStartDCNforMoreThanOneDC());
+		}
 		waitforVisibilityofElementLocated(txt_EndDCN, 10);
 		clickonButton(btn_Save);
-		System.out.println("*** Save clicked in BatchLookup ***");
 		waitforTexttobePresentinElementLocated(msg_BatchSavedConfirmation, 25, "Batch saved successfully.");
+		Log.info("********* Batches tobe Scanned Save message: " + gettingWebElement(msg_BatchSavedConfirmation).getText());		
 		switchtoDefaultFrame();
 		switchtoFrame(frame_HomePage);
 		clickonButton(icon_BatchLookupClose);
 		Log.info(" *************** Batches tobe Scanned Execution Completed *************** ");
+		Log.info("");
 	}
-
+	
+	public String generateStartDCNforMoreThanOneDC() throws Exception {
+		
+		return "410" + generatingRandomNumbers().replace(9, 12, "").toString() + "001";		
+	}
+	
+	private void NavigatetoCreateBatch() throws Exception {
+		
+		batchentrypage.NavigatetoHomePage();
+		waitforInVisibilityofElementLocated(elementtoInvisible, 15);
+		javascript_scrollIntoView(widget_ScanBatch);
+		waitforVisibilityofElementLocated(widget_ScanBatch, 20);
+		clickonButton(widget_ScanBatch);
+		waitforVisibilityofElementLocated(txt_joborBatchCode, 25);
+	}
 }
