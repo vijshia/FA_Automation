dba922aec53b9c9187a52628df5e11a5631596aa
Vijay Sivasubramanian <37892349+vijshia@users.noreply.github.com>
2020-01-07 20:05:48 +0530

    Update WebContext.java
    updating WebContext

--- eclipse-workspace/TestArt/src/test/java/com/FA/framework/WebContext.java
+++ eclipse-workspace/TestArt/src/test/java/com/FA/framework/WebContext.java
@@ -3,17 +3,15 @@
  */
 package com.FA.framework;
 
-//import java.io.File;
-//import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-//import org.openqa.selenium.support.ui.WebDriverWait;
 import static com.FA.app.tests.baseFactory.filepath;
 import com.FA.framework.Log;
 
@@ -24,46 +22,19 @@ import com.FA.framework.Log;
 public class WebContext {
 
 	public static WebDriver driver;
-	
-//	  public static WebDriverWait wait; 
+	public static String BE_BatchType;
+	public static String BE_Facility;
+	public static String batchCode;
+	public static String episodeID;
+	public static String invoice;
+	public static String piErrorMessage;
+	public static String codingErrorMessage;
+	public static String peErrorMessage;
+	public static String billingErrorMessage;
 
-	  public static	String batchType; 
-	  public static String facilityId; 
-	  public static String batchCode;
-	  public static String episodeID;
-	 
-
-	protected static final String TEST_DATA_PROPERTY_PATH = "\\Inputs\\config - Copy.properties";
-
-	/*
-	 * public WebContext() {
-	 * 
-	 * 
-	 * try { getPropertyvalues(); } catch (Exception e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * private void getPropertyvalues() throws Exception {
-	 * 
-	 * PropertyReader propreader = new PropertyReader(); try { for (String key :
-	 * propreader.getProperty(filepath +
-	 * TEST_DATA_PROPERTY_PATH).stringPropertyNames()) { String value =
-	 * propreader.getProperty(filepath + TEST_DATA_PROPERTY_PATH).getProperty(key);
-	 * if (key.equalsIgnoreCase("browser")) { browsertoLaunch = value; } else if
-	 * (key.equalsIgnoreCase("login_username")) { userName = value; } else if
-	 * (key.equalsIgnoreCase("login_password")) { password = value; } else if
-	 * (key.equalsIgnoreCase("value_facilityId")) { facilityId = value; } } } catch
-	 * (IOException e) { e.printStackTrace(); } }
-	 */
+	public static final String TEST_DATA_PROPERTY_PATH = "\\Inputs\\config - Copy.properties";
 
 	public void LaunchBrowser(String browsertolaunch, String url) throws Exception {
-
-		/*
-		 * WebContext webcontext = new WebContext(); try {
-		 * webcontext.getPropertyvalues(); } catch (Exception e) { e.printStackTrace();
-		 * }
-		 */
 
 		if (browsertolaunch.equalsIgnoreCase("ie")) {
 			System.setProperty("webdriver.ie.driver", filepath + "\\Drivers\\IEDriverServer.exe");
@@ -71,7 +42,9 @@ public class WebContext {
 			Log.info(" InternetExplorer Browser Launched ");
 		} else if (browsertolaunch.equalsIgnoreCase("ch")) {
 			System.setProperty("webdriver.chrome.driver", filepath + "\\Drivers\\chromedriver.exe");
-			driver = new ChromeDriver();
+			ChromeOptions choptions = new ChromeOptions();
+			choptions.setAcceptInsecureCerts(true);
+			driver = new ChromeDriver(choptions);
 			Log.info(" Chrome Browser Launched ");
 		} else if (browsertolaunch.equalsIgnoreCase("ff")) {
 			System.setProperty("webdriver.gecko.driver", filepath + "\\Drivers\\geckodriver.exe");
@@ -88,4 +61,23 @@ public class WebContext {
 		driver.get(url);
 	}
 
+	/*
+	 * public WebContext() {
+	 * 
+	 * try { getPropertyvalues(); } catch (Exception e) { e.printStackTrace(); }
+	 * 
+	 * }
+	 * 
+	 * private void getPropertyvalues() throws Exception {
+	 * 
+	 * PropertyReader propreader = new PropertyReader(); try { for (String key :
+	 * propreader.getProperty(filepath +
+	 * TEST_DATA_PROPERTY_PATH).stringPropertyNames()) { String value =
+	 * propreader.getProperty(filepath + TEST_DATA_PROPERTY_PATH).getProperty(key);
+	 * if (key.equalsIgnoreCase("browser")) { browsertoLaunch = value; } else if
+	 * (key.equalsIgnoreCase("login_username")) { userName = value; } else if
+	 * (key.equalsIgnoreCase("login_password")) { password = value; } else if
+	 * (key.equalsIgnoreCase("value_facilityId")) { facilityId = value; } } } catch
+	 * (IOException e) { e.printStackTrace(); } }
+	 */
 }
